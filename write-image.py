c04c99f44da22146501d919cd5d649e2a607fbe1
Reuben Kraft <reuben.kraft@gmail.com>
2019-05-23 16:31:07 -0400

    Update write-image.py

--- write-image.py
+++ write-image.py
@@ -1,45 +1,30 @@
-import vtk
+#! ./local/bin/python
 
-# create a rendering window and renderer
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren)
+from vtk import (vtkSphereSource, vtkPolyDataMapper, vtkActor, vtkRenderer,
+        vtkRenderWindow, vtkWindowToImageFilter, vtkPNGWriter)
 
-# create a renderwindowinteractor
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
+sphereSource = vtkSphereSource()
+mapper = vtkPolyDataMapper()
+mapper.SetInputConnection(sphereSource.GetOutputPort())
 
-# create source
-source = vtk.vtkSphereSource()
-source.SetCenter(0,0,0)
-source.SetRadius(5.0)
-
-# mapper
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInput(source.GetOutput())
-
-# actor
-actor = vtk.vtkActor()
+actor = vtkActor()
 actor.SetMapper(mapper)
 
-# color the actor
-actor.GetProperty().SetColor(1,0,0) # (R,G,B)
+renderer = vtkRenderer()
+renderWindow = vtkRenderWindow()
+renderWindow.SetOffScreenRendering(1)
+renderWindow.AddRenderer(renderer)
 
-# assign actor to the renderer
-ren.AddActor(actor)
+renderer.AddActor(actor)
+renderer.SetBackground(1, 1, 1)
 
-renWin.Render()
+renderWindow.Render()
 
-# screenshot code:
-w2if = vtk.vtkWindowToImageFilter()
-w2if.SetInput(renWin)
-w2if.Update()
+windowToImageFilter = vtkWindowToImageFilter()
+windowToImageFilter.SetInput(renderWindow)
+windowToImageFilter.Update()
 
-writer = vtk.vtkPNGWriter()
-writer.SetFileName("screenshot.png")
-writer.SetInput(w2if.GetOutput())
+writer = vtkPNGWriter()
+writer.SetFileName("sphere.png")
+writer.SetInputConnection(windowToImageFilter.GetOutputPort())
 writer.Write()
-
-# enable user interface interactor
-iren.Initialize()
-iren.Start()
